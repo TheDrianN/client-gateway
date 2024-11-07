4b07910376ef8b2d86c109d857ed1751a0c880cf
TheDrianN <edelgadovidarte@gmail.com>
2024-11-06 23:52:42 -0500

    create dti

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,14 +2,14 @@ import { Controller, Post, Body, Inject, Get } from '@nestjs/common';
 import { AUTH_SERVICE } from 'src/config';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError, throwError } from 'rxjs';
+import { CreateAuthDto } from './dto/create-auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(@Inject(AUTH_SERVICE) private readonly authClient: ClientProxy) {}
 
   @Post('/login')
-  login(@Body() loginDto: { document: string; password: string }) {
-    console.log(loginDto)
+  login(@Body() loginDto: CreateAuthDto) {
     return this.authClient.send('loginAuth', loginDto).pipe(
       catchError(err => {
         throw new RpcException(err.message);
@@ -18,8 +18,7 @@ export class AuthController {
   }
 
   @Post('/validation')
-  validation(@Body() loginDto: { document: string; password: string }) {
-    console.log(loginDto)
+  validation(@Body() loginDto: CreateAuthDto) {
     return this.authClient.send('validation', loginDto).pipe(
       catchError(err => {
         throw new RpcException(err.message);
--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,13 @@
-export class CreateAuthDto {}
+import { IsString, IsNotEmpty, Length } from 'class-validator';
+
+export class CreateAuthDto {
+    @IsString()
+    @IsNotEmpty()
+    @Length(8, 20)
+    document: string;
+  
+    @IsString()
+    @IsNotEmpty()
+    @Length(9)
+    password: string;
+}
